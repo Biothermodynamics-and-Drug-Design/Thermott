3fd9b6f2fc02109c0f664d70db7f22316a7bead5
Marius Gedgaudas <gedgaudas.marius@gmail.com>
2022-06-10 16:46:49 +0300

    misc

--- server/main.py
+++ server/main.py
@@ -1,22 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.api import api
-
-# from app.metrics import enable_metrics, metrics_router
 
 app = FastAPI()
 app.include_router(api.router)
-# app.include_router(metrics_router)
-
-
-# Prometheus metrics
-# enable_metrics(app)
-
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=False,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
--- server/main_debug.py
+++ server/main_debug.py
@@ -6,7 +6,4 @@ from main import app
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=5000)
 
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-
 
